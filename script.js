d0c26df553a1e95aeeb59a3679199c2d64a4c978
rafa1510 <rcestti78@gmail.com>
2023-10-05 00:38:27 -0400

    Add function playRound()

--- script.js
+++ script.js
@@ -7,10 +7,49 @@ function getComputerChoice()
     }
     else if (randomNumber > 33 && randomNumber <= 66)
     {
-        return("PAPER")
+        return("PAPER");
     }
     else if (randomNumber > 66 && randomNumber <= 99)
     {
-        return("SCISSORS")
+        return("SCISSORS");
+    }
+}
+function playRound(playerSelection, computerChoice)
+{
+    let casePlayerSelection = playerSelection.toUpperCase();
+    if (casePlayerSelection == computerChoice)
+    {
+        console.log("It's a tie!");
+        return("Tie");
+    }
+    else if (casePlayerSelection == "ROCK" && computerChoice == "PAPER")
+    {
+        console.log("You lose! Paper beats Rock");
+        return("Computer");
+    }
+    else if (casePlayerSelection == "ROCK" && computerChoice == "SCISSORS")
+    {
+        console.log("You win! Rock beats Scissors");
+        return("Player");
+    }
+    else if (casePlayerSelection == "PAPER" && computerChoice == "ROCK")
+    {
+        console.log("You win! Paper beats Rock");
+        return("Player");
+    }
+    else if (casePlayerSelection == "PAPER" && computerChoice == "SCISSORS")
+    {
+        console.log("You lose! Scissors beats Paper");
+        return("Computer");
+    }
+    else if (casePlayerSelection == "SCISSORS" && computerChoice == "ROCK")
+    {
+        console.log("You lose! Rock beats Scissors");
+        return("Computer");
+    }
+    else if (casePlayerSelection == "SCISSORS" && computerChoice == "PAPER")
+    {
+        console.log("You win! Scissors beats Paper");
+        return("Player");
     }
 }
